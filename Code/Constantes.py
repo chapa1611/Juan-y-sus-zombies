0b233d93ec99d6738067bddbb2bd69a421d7f814
astroobot <Beginner_Programmer@outlook.es>
2024-03-08 06:43:40 -0500

    initial files

---
+++ Code/Constantes.py
@@ -0,0 +1,15 @@
+"""Este modulo se encarga de almacenar todos los valores fundamentales que se usaran en el resto del codigo como son las imagenes, sonido y valores"""
+
+import pygame 
+
+Tamaño_pantalla = ancho, altura = 800, 600
+
+
+#imagenes
+Imagen_jugador = null
+Imagen_zombie = null
+Imagen_tumba = null
+Fondo_inicio = null
+Fondo_juego = null
+Fondo_fin_juego = null
+
