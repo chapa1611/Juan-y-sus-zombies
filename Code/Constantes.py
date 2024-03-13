9c7d2e4348774857c395cf2bda4f889fdd246f9c
JuanDCespedes <159020969+JuanDCespedes@users.noreply.github.com>
2024-03-13 08:04:55 -0500

    Update Constantes.py

--- Code/Constantes.py
+++ Code/Constantes.py
@@ -1,15 +1,17 @@
 """Este modulo se encarga de almacenar todos los valores fundamentales que se usaran en el resto del codigo como son las imagenes, sonido y valores"""
 
-import pygame 
+import pygame
 
 Tamaño_pantalla = ancho, altura = 800, 600
 
-
 #imagenes
-Imagen_jugador = null
-Imagen_zombie = null
+jugador = [[pygame.image.load("imagenes/Juaniz1.png"), pygame.image.load("imagenes/Juaniz2.png"), pygame.image.load("imagenes/Juaniz3.png")],
+           [pygame.image.load("imagenes/Juan1.png"), pygame.image.load("imagenes/Juan2.png"), pygame.image.load("imagenes/Juan3.png")]]
+"""Imagen_zombie = null
 Imagen_tumba = null
 Fondo_inicio = null
 Fondo_juego = null
-Fondo_fin_juego = null
+Fondo_fin_juego = null"""
+
+
 
