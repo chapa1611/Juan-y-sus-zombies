394c4836443d8e9adc5c53fc4e86b48ce6065890
JuanDCespedes <159020969+JuanDCespedes@users.noreply.github.com>
2024-03-13 08:05:11 -0500

    Update Pantalla.py

--- Code/Pantalla.py
+++ Code/Pantalla.py
@@ -1,1 +1,31 @@
-"""Este modulo almacena el contenido de las diferentes pantallas (menu de inicio, pantalla de juego y pantalla de fin de juego)"""
+
+import pygame
+from pygame.locals import *
+import sys
+from Jugador import *
+from Constantes import *
+
+size=width, height=800, 600
+screen=pygame.display.set_mode(size)
+
+def main():
+    pygame.init()
+    background_image=pygame.image.load("imagenes/negro.jpg")
+    background_rect=background_image.get_rect()
+    pygame.display.set_caption("Inserte titulo creativo xd")
+    juan=Juan(size)
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                sys.exit()
+        
+        Jugador.animar()
+
+        screen.blit(background_image, background_rect)
+        screen.blit(juan.imagen, juan.rect)
+        pygame.display.update()
+        pygame.time.delay(10)
+
+if __name__=="__main__":
+    main()
