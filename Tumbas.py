b9136298a0bf5eaa160a30933bd4179d90368633
astroobot <Beginner_Programmer@outlook.es>
2024-03-17 14:38:07 -0500

    upload tumbas code

--- Tumbas.py
+++ Tumbas.py
@@ -2,6 +2,7 @@
 import sys 
 import pygame 
 import random
+import time
 from pygame.sprite import Sprite
 
 class Tumbas(Sprite):
@@ -11,26 +12,51 @@ class Tumbas(Sprite):
         self.base_image = pygame.image.load("Tumbas.jpg")
         self.image = self.base_image
         self.rect = self.image.get_rect()
-        self.velocidad = [-1,0]
-        self.rect.x = size[0]-100#random.randint(0,contenedor[0]-50)
+        self.velocidad = 50
+        self.rect.x = size[0] + 236
         self.rect.y = 300
 
-    def move(self): 
-          self.velocidad[0] *= 1
-          self.rect = self.rect.move(self.velocidad) 
+    def update(self): 
+
+          self.rect.x -= self.velocidad
+          if self.rect.x + 236 < 0: #236 es el ancho de la imagen de referencia
+            self.rect.x = size[0] + 236
+          self.mano()
+         
+         
+    def mano(self):
+        if self.rect.x <= size[0]:
+            num = random.randint(1,5)
+            pygame.time.wait(400) 
+            if num == 5:
+                self.image = pygame.image.load("Tumbasmano.jpg")
+            else: 
+                self.image = pygame.image.load("Tumbas.jpg")
+    
+ """   def detectar(self, rect):
+        for t in range(self.rect.bottomleft, self.rect.topleft):
+            if t == rect.left or t == rect.right:
+                print('choque')"""
         
 
+    
+ """           
 size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 tumbas = Tumbas(size)
+
 pygame.init()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: 
-            sys.exit()               
+            sys.exit() 
+    screen.fill((0,0,0))
+    
+    tumbas.update() 
+             
     screen.blit(tumbas.image, tumbas.rect)
-    tumbas.move()
+    screen.blit(tumbe.image, tumbe.rect)
     pygame.display.update()
-    pygame.display.clear()
-    pygame.time.delay(10)
+ 
+    pygame.time.delay(10)"""
